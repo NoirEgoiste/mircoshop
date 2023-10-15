5d7e985bb362392e96de0f65532b92c802dbac6d
Noir_Egoiste <dimon0.0@yandex.ru>
2023-10-15 22:28:56 +0300

    Create posts model

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -2,8 +2,10 @@ __all__ = (
     "Base",
     "Product",
     "User",
+    "Post",
 )
 
 from .base import Base
 from core.models.product import Product
 from core.models.user import User
+from core.models.post import Post
